370e4fa8317ec4be0ed71df5f7da3bd8eb47a2be
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-11-03 12:31:02 +0000

    Add Community Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,19 @@ import Layout from "@theme/Layout";
 
 import Welcome from "@site/src/components/Welcome";
 import About2 from "@site/src/components/About2";
+import Community from "@site/src/components/Community";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title} | ${siteConfig.tagline}`}
-      description="Description will go into a meta tag in <head />"
+      description="Open Source Fleet Management for Edge Devices - Manage thousands of edge devices with GitOps workflows, built-in security, and centralized control"
     >
       <main>
         <Welcome />
         <About2 />
+        <Community />
       </main>
     </Layout>
   );
